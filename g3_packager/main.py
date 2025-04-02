ac481808b5fae810b76276d36e00ca17e8f249c6
Jonah Lee <jonahjacqueslee@gmail.com>
2025-04-01 17:09:07 -0700

    fix: stray argument

--- g3_packager/main.py
+++ g3_packager/main.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     os.makedirs(out_dir, exist_ok=True)
 
     pipe = core.G3Pipeline()
-    generator = ScanFrameGenerator(verbose=True)
+    generator = ScanFrameGenerator()
 
     pipe.Add(generator)
     pipe.Add(core.G3Writer, filename=os.path.join(out_dir, 'testfile.g3'))
