746427f53680a88f7478330c3206e8a78746fe69
Jonah Lee <jonahjacqueslee@gmail.com>
2025-04-01 17:03:17 -0700

    fix: infinite scan frames

--- g3_packager/main.py
+++ g3_packager/main.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
         def __init__(self, roach_ids: tuple[int]=None):
             self.roach_ids: tuple[int] = roach_ids if roach_ids is not None else (1, 2, 3, 4, 5)
             self.roaches: dict = self._load_roaches()
+            self.done = False
 
         def _load_roaches(self):
             """Loads a RoachPass objet for each Roach ID in self.roach_ids
@@ -28,9 +29,14 @@ if __name__ == '__main__':
             """
             roaches = {roach_id : RoachPass(RoachID(roach_id), ScanPass.ALL, use_rejects_file=False)
                        for roach_id in self.roach_ids}
+
             return roaches
 
         def __call__(self, frame):
+            if self.done:
+                return []
+
+            self.done = True
             out = core.G3Frame(core.G3FrameType.Scan)
             return out
 
@@ -38,10 +44,10 @@ if __name__ == '__main__':
     os.makedirs(out_dir, exist_ok=True)
 
     pipe = core.G3Pipeline()
-    generator = ScanFrameGenerator()
+    generator = ScanFrameGenerator(verbose=True)
 
     pipe.Add(generator)
-    pipe.Add(core.G3Writer, filename=os.path.join(out_dir, 'testing.g3'))
+    pipe.Add(core.G3Writer, filename=os.path.join(out_dir, 'testfile.g3'))
 
     pipe.Run()
 
