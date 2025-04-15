f826cd87a2d125b1f226cd20af075e4f29a231e5
Jonah Lee <jonahjacqueslee@gmail.com>
2025-04-15 11:46:43 -0700

    rename df_iqangle to lowercase

--- g3_utils/__init__.py
+++ g3_utils/__init__.py
@@ -2,7 +2,7 @@ from .coords import add_radec_astropy, add_radec_so3g
 from .maps import BLASTTNG_SITE, SingleMapBinner, MapBinner
 from .tools import (kid_string, FrameCounter, NthFrameGrabber, FirstFrameGrabber,
                     LastFrameGrabber, PlotRaDec, GenericPlotter, TimeStreamPlotter)
-from .signal import (DetectorStats, df_IQangle, add_cal_lamp_df, AddScanDF,
+from .signal import (DetectorStats, df_iqangle, add_cal_lamp_df, AddScanDF,
                      AddSingleKidDF, naive_normalize_df, NormalizeDF, remove_common_mode)
 
 __all__ = [
@@ -17,6 +17,6 @@ __all__ = [
     'PlotRaDec', 'GenericPlotter', 'TimeStreamPlotter',
 
     # signal
-    'DetectorStats', 'df_IQangle', 'add_cal_lamp_df', 'AddScanDF',
+    'DetectorStats', 'df_iqangle', 'add_cal_lamp_df', 'AddScanDF',
     'AddSingleKidDF', 'naive_normalize_df', 'NormalizeDF', 'remove_common_mode'
 ]
